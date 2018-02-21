96708f4b77191a67a9ada051b9379c4bab6ffc8b
MadProgrammerGR <giorgozombo@yahoo.gr>
2018-02-21 13:29:04 +0200

    ligo kalyteres syntages

--- AAY Internet Of Things/FridgeApps/Recipes.Designer.cs
+++ AAY Internet Of Things/FridgeApps/Recipes.Designer.cs
@@ -36,17 +36,26 @@
             // 
             // listBox1
             // 
+            this.listBox1.BackColor = System.Drawing.Color.LightYellow;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(121, 46);
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Items.AddRange(new object[] {
+            "Μακαρόνια με κιμά",
+            "Μουσακάς",
+            "Παστίτσιο",
+            "Χταπόδι λεμονάτο"});
+            this.listBox1.Location = new System.Drawing.Point(201, 48);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(295, 238);
+            this.listBox1.Size = new System.Drawing.Size(296, 229);
             this.listBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightYellow;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.label1.Location = new System.Drawing.Point(162, 10);
+            this.label1.Location = new System.Drawing.Point(261, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(151, 33);
             this.label1.TabIndex = 2;
@@ -54,20 +63,41 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(3, 58);
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.button1.Location = new System.Drawing.Point(31, 39);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(151, 23);
             this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Διαθέσιμα Υλικά";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // checkedListBox1
             // 
+            this.checkedListBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(0, 78);
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "3 κ.σ.ελαιόλαδο",
+            "750 γρ.κιμάς μοσχαρίσιος",
+            "2 κρεμμύδια, ψιλοκομμένα",
+            "1 σκ.σκόρδο, ψιλοκομμένο",
+            "1 κ.σ.ζάχαρη",
+            "1 κ.σ.ξερό θυμάρι",
+            "2 κ.σ.πελτέ ντομάτας",
+            "100 ml κόκκινο κρασί",
+            "200 ml νερό",
+            "1 κύβο ζωμό μοσχαριού ή κοτόπουλο",
+            "1 στικ κανέλα",
+            "2 φύλλα δάφνης",
+            "3 κόκκους μπαχάρι",
+            "1 κονσέρβα τομάτα κονκασέ(400 γρ.)",
+            "1 πακέτο μακαρόνια σπαγγέτι(500 γρ.)",
+            "100 ml κρέμα γάλακτος",
+            "παρμεζάνα για το σερβίρισμα"});
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 68);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(120, 214);
+            this.checkedListBox1.Size = new System.Drawing.Size(183, 214);
             this.checkedListBox1.TabIndex = 5;
             this.checkedListBox1.Visible = false;
             // 
--- AAY Internet Of Things/FridgeApps/Recipes.cs
+++ AAY Internet Of Things/FridgeApps/Recipes.cs
@@ -15,11 +15,19 @@ namespace AAY_Internet_Of_Things.FridgeApps
         public Recipes()
         {
             InitializeComponent();
+            int[] owned = new int[] {0,2,5,6,7,10,11,12,13};
+            foreach(int i in owned) checkedListBox1.SetItemCheckState(i, CheckState.Checked);
+            checkedListBox1.ItemCheck += checkedListBox1_ItemCheck;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             checkedListBox1.Visible = !checkedListBox1.Visible;
+        }
+
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            e.NewValue = e.CurrentValue; //de ginetai na kanoun check(mono osa exei to psygeio mesa menoun checked)
         }
     }
 }
